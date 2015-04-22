6f916c6d6873c24ab24f2ffc87f571383d9cd4a3
Tlapička <marek@tlapicka.net>
2015-04-22 14:51:09 +0200

    První nástřel

---
+++ content/spseol.md
@@ -0,0 +1,11 @@
+Title: VOŠ s SPŠE Olomouc na GitHubu
+Date: 2015-04-22 13:48
+Category: Blog
+Tags: github
+Author: Marek Nožka
+
+
+Na této stránce bych vás rád upozornil na několik našich zajímavých repozitářů:
+
+* [CzechFlaskDoc](): <http://spseol.github.io/CzechFlaskDoc/>
+* [PyQt-4-Documentation](): <http://spseol.github.io/PyQt-4-Documentation/>
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'spseol.cz'
+SITENAME = u'spseol.github.io'
+SITEURL = 'spseol.github.io'
+SITEURL = ''
+
+PATH = 'content'
+THEME = 'themes/simplegrey'
+TIMEZONE = 'Europe/Prague'
+
+
+DEFAULT_LANG = u'cz'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('github.com/spseol', 'https://github.com/spseol/'),
+         ('www.spseol.cz', 'http://www.spseol.cz/'),
+         ('~nozka', 'http://hroch.spseol.cz/~nozka/'),)
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/spseol/'),
+          ('Facebook', 'https://cs-cz.facebook.com/spseol'),
+          ('G+', 'https://plus.google.com/118347382558771670752'),
+          ('Pelican', 'http://blog.getpelican.com/'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
